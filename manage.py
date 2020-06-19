bcdb9a10ef41afbdc5c159276c98e9b2775266b3
yueht17 <yueht17@mails.tsinghua.edu.cn>
2020-06-19 14:14:34 +0800

    添加了生成虚假数据的功能

--- manage.py
+++ manage.py
@@ -14,5 +14,24 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def generate_fake():
+    sql_file = open('generate_fake.sql', 'r', encoding='utf-8')
+    sql_command = ''
+    for line in sql_file:
+        if not line.startswith('--') and line.strip('\n'):
+            sql_command += line.strip('\n')
+            if sql_command.endswith(';'):
+                try:
+                    db.session.execute(sql_command)
+                    db.session.commit()
+                except:
+                    print('Ops')
+                finally:
+                    sql_command = ''
+    print("[Info]:Generate fake info Done!")
+
+
 if __name__ == '__main__':
     manager.run()
